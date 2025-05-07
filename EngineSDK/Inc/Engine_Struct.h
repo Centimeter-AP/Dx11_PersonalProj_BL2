de3ac2be7893e6e7922193c85a182f2de589869f
CM <bcy4601@gmail.com>
2025-05-07 15:56:20 +0900

    Initial Commit

---
+++ EngineSDK/Inc/Engine_Struct.h
@@ -0,0 +1,48 @@
+#pragma once
+
+namespace Engine
+{
+	typedef struct tagEngineDesc 
+	{
+		HWND			hWnd;
+		bool			isWindowed;
+		unsigned int	iWinSizeX;
+		unsigned int	iWinSizeY;
+		unsigned int	iNumLevels;		
+	}ENGINE_DESC;
+
+
+
+
+	typedef struct ENGINE_DLL tagVertexPositionTexcoord
+	{
+		XMFLOAT3		vPosition;
+		XMFLOAT2		vTexcoord;
+
+		static const unsigned int					iNumElements = {2};
+
+		static const D3D11_INPUT_ELEMENT_DESC		Elements[iNumElements];
+	}VTXPOSTEX;
+
+	typedef struct ENGINE_DLL tagVertexPositionNormalTexcoord
+	{
+		XMFLOAT3		vPosition;
+		XMFLOAT3		vNormal;
+		XMFLOAT2		vTexcoord;
+
+		static const unsigned int					iNumElements = { 3 };
+		static const D3D11_INPUT_ELEMENT_DESC		Elements[iNumElements];
+	}VTXNORTEX;
+
+	typedef struct ENGINE_DLL tagVertexMesh
+	{
+		XMFLOAT3		vPosition;
+		XMFLOAT3		vNormal;
+		XMFLOAT3		vTangent;
+		XMFLOAT2		vTexcoord;
+
+		static const unsigned int					iNumElements = { 4 };
+		static const D3D11_INPUT_ELEMENT_DESC		Elements[iNumElements];
+	}VTXMESH;
+
+}
